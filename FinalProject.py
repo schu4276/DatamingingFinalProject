9649b1cee39f747a1ec5b2a81c7950b910f5fb8c
schu4276 <69864668+schu4276@users.noreply.github.com>
2020-11-06 12:31:17 -0600

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -7,4 +7,11 @@ Created on Fri Nov  6 12:02:40 2020
 
 import pandas as pd
 
+# Defining dataset
 UFO_df = pd.read_csv('/DataMining/lab2/complete.csv')
+# Cleaning data, there are quite a few pieces missing from the data set, so this 
+# Needs to be addressed
+UFO_df = UFO_df.dropna(how='all') # dropping all columns that are completely empty
+UFO_df = UFO_df.drop(columns = 'city') # dropping city column, too much missing data
+
+UFO_df.info
