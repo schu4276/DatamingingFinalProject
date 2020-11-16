7cbb94a839b3ed387ba12ab1389bff9c7644be70
schu4276 <69864668+schu4276@users.noreply.github.com>
2020-11-16 12:31:50 -0600

    attempted to add Nearest Neighbor
    I have to figure out better how Nearest Neighbor works, but the two separate linear regressions are in there (with latitude/longitude) I also changed the Linear regression so that it only uses the variables found in stepwise reduction.

--- FinalProject.py
+++ FinalProject.py
@@ -59,18 +59,91 @@ sns.heatmap(corr, annot=False, fmt= ".1f", cmap= "RdBu", center=0, ax=ax)
 plt.show()
 
 
-## Multiple Linear Regression 
+## Partitioning Data for Stepwise Regression 
 US_df_loc = US_df.iloc[0:1000]
 US_df_loc.info()
 unwanted = ['city','country', 'latitude', 'longitude','state', 'combined']
 
-print(predictors)
-outcome = ['latitude', 'longitude']
+X = US_df_loc.drop(columns = unwanted) 
+y = US_df_loc['longitude']
+X.shape
+y.shape
+train_X, valid_X, train_y, valid_y = train_test_split(X,y, test_size=0.4, random_state=1)
+# print coefficients
 
+
+
+
+# print performance measures (training data)
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, LassoCV, BayesianRidge
+import statsmodels.formula.api as sm
+import matplotlib.pylab as plt
+
+from dmba import regressionSummary, exhaustive_search
+from dmba import backward_elimination, forward_selection, stepwise_selection
+from dmba import adjusted_r2_score, AIC_score, BIC_score
+
+import math
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score, roc_curve, auc
+import matplotlib.pylab as plt
+
+from dmba import regressionSummary, classificationSummary
+from dmba import liftChart, gainsChart
+
+
+US_lm = LinearRegression()
+US_lm .fit(train_X, train_y)
+
+regressionSummary(train_y, US_lm.predict(train_X))
+def train_model(variables):
+    if len(variables) == 0:
+        return None
+    model = LinearRegression()
+    model.fit(train_X[variables], train_y)
+    return model
+
+def score_model(model, variables):
+    if len(variables) == 0:
+        return AIC_score(train_y, [train_y.mean()] * len(train_y), model, df=1)
+    return AIC_score(train_y, model.predict(train_X[variables]), model)
+
+
+best_model, best_variables = stepwise_selection(train_X.columns, train_model, score_model, 
+                                 verbose=True)
+print(best_variables)
+regressionSummary(valid_y, best_model.predict(valid_X[best_variables]))
+# lift chart for stepwise model
+
+pred_v = pd.Series(best_model.predict(valid_X[best_variables]))
+pred_v = pred_v.sort_values(ascending=False)
+
+fig, axes = plt.subplots(nrows=1, ncols=2)
+ax = gainsChart(pred_v, ax=axes[0])
+ax.set_ylabel('Cumulative Price')
+ax.set_title('Cumulative Gains Chart')
+
+ax = liftChart(pred_v, ax=axes[1], labelBars=False)
+ax.set_ylabel('Lift')
+
+plt.tight_layout()
+plt.show()
+
+
+## Multiple Linear Regression With Seperate Lat and Long
+US_df_loc = US_df.iloc[0:1000]
+
+
+#outcome = ['latitude']
+outcome = ['longitude']
 
 from sklearn.model_selection import train_test_split
 
-X = US_df_loc.drop(columns = unwanted) 
+X = US_df_loc[best_variables]
 y = US_df_loc['latitude']
 X.shape
 y.shape
@@ -86,3 +159,56 @@ US_df_loc
 
 from dmba import regressionSummary
 regressionSummary(train_y, UFO_lm.predict(train_X))
+
+## Using Nearest Neighbor
+US_df.shape_changing
+# print coefficients
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+from sklearn.neighbors import NearestNeighbors
+
+# Transform the full dataset
+UFO_df = pd.read_csv('/DataMining/lab2/complete.csv')
+low_memory=False
+trainData, validData = train_test_split(US_df, test_size = 0.4, random_state = 1)
+
+
+scaler = preprocessing.StandardScaler()
+scaler.fit(trainData[['shape_changing', 'duration']])
+
+# Transforming dataset
+Norm = pd.concat([pd.DataFrame(scaler.transform(US_df[['shape_changing', 'duration']]), columns=['duration','shape_changing']), US_df[['latitude', 'longitude']]], axis=1)
+trainData.index
+
+trainNorm = Norm.iloc[trainData.index]
+validNorm = Norm.iloc[validData.index]
+newUFONorm = pd.DataFrame(scaler.transform(newUFO),columns=['duration','shape_changing'])
+
+
+# Use k-nearest neighbor
+newUFO = pd.DataFrame([{'duration': 30, 'shape_changing':0 }])
+knn = NearestNeighbors(n_neighbors=3)
+knn.fir(trainNorm[['duration', 'shape_changing']])
+distances, indicies = knn.kneighbors(newUFONorm)
+print(trainNorm.iloc[indicies[0], :])
+
+
+train_X = trainNorm[['shape_cigar','shape_disk','shape_circle','shape_changing', 'duration']]
+train_y = trainNorm['latiude']
+valid_X = validNorm[['shape_cigar', 'shape_disk','shape_circle','shape_changing','duration']]
+valid_y = validNorm['latiude']
+
+# Train a classifier for different values of K
+results = []
+for k in range(1,15):
+    knn = KNeighborsClassifier(n_neighbors=k).fit(train_X, train_y)
+    results.append({
+        'k':k,
+        'accuracy': accuracy_score(valid_y, knn.predict(valid_X))
+    })
+# Convert results to a pandas df
+results = pd.DataFrame(results)
+print(results)
+
+
