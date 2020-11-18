76de298c2c09245c30202add1cb4adc00314620d
Utkarsh Kumar <kumar375@morris.umn.edu>
2020-11-18 02:14:56 -0600

    Improve dataframe cleaning and import statements

--- FinalProject.py
+++ FinalProject.py
@@ -6,51 +6,72 @@ Created on Fri Nov  6 12:02:40 2020
 """
 
 import pandas as pd
+
+# Defining dataset and exploring data
+UFO_df = pd.read_csv(r'UFO/complete.csv', low_memory=False)
+
+# To see all columns
+pd.set_option('display.max_columns', None)
+
+UFO_df.info()
+
+### Cleaning the data to have a cleaned data set for the rest of the project ###
+
+# These are the rows where the unnamed column has values. This unnamed columns exists
+# because all 195 of these rows have data in the wrong columns or missing data.
+badRows = UFO_df[UFO_df['Unnamed: 11'].notnull()]
+print(badRows)
+
+# Drop those rows by passing their index to .drop() and using axis=0.
+# Also drop the Unnamed column which is now full of NA values
+UFO_df.drop(badRows.index, axis=0, inplace=True)
+UFO_df.drop(columns=['Unnamed: 11'], inplace=True)
+
+# Drop all rows with NA values
+UFO_df.dropna(inplace=True)
+# Rename duration to remove unit. Rename shape, so it doesn't clash with DataFrame.shape method
+UFO_df.rename(columns={'duration (seconds)': 'duration', 'shape': 'UFO_shape'}, inplace=True)
+
+# Convert data type of duration and latitude columns from object to float64 
+UFO_df['duration'] = pd.to_numeric(UFO_df.duration)
+UFO_df['latitude'] = pd.to_numeric(UFO_df.latitude)
+
+UFO_df.info()
+
+# We are removing datetime for now, but really we might use it later.
+# Drop the duration column given in hours/min, since there is a column of duration in seconds.
+# We are not interested in the date sightings were posted online or the comments
+# associated with those sightings.
+UFO_df_cleaned = UFO_df.drop(columns=['datetime', 'duration (hours/min)', 'comments', 'date posted'])
+
+
+######### Focusing on US UFO Sighting #########
+
+# New Dataframe with just sighting in United States.
+US_df = UFO_df_cleaned[UFO_df_cleaned.country == 'us']   
+
+# Confirming the country column only has United States sightings.
+US_df.country.value_counts()
+
+# Turn shapes into dummy variables
+US_df = pd.get_dummies(US_df, columns=['UFO_shape'], prefix_sep='_') 
+# Create a new column that combines latitude and longitude
+US_df['combined'] = list(zip(US_df.latitude, US_df.longitude))
+
+
+######### Correlation Table and Visualization #########
+
+import seaborn as sns
 import matplotlib.pylab as plt
 
 
-# Defining dataset and exploring data
-UFO_df = pd.read_csv('/DataMining/lab2/complete.csv')
-low_memory=False
-# Cleaning the data to have a cleaned data set for the rest of the project
-# Cleaning data, there are quite a few pieces missing from the data set, so this 
-# Needs to be addressed
-
-US_df = UFO_df.drop(columns = ['duration (hours/min)', 'Unnamed: 11','comments','date posted','datetime'])
-
-US_df.country = US_df[US_df.country == 'us']   
-
-
-US_df.country.value_counts()
-US_df = US_df.dropna() # droping columns w/ NA values
-
-# changing duration to numerical value
-US_df = US_df.rename(columns={'duration (seconds)': 'duration'}) 
-US_df.duration =  US_df.duration.astype('float64')
-
-US_df.latitude
-# getting rid of values that that were messed up/ invalid 
-US_df = US_df[~US_df.latitude.str.contains('/')]
-US_df = US_df[~US_df.latitude.str.contains('q')]
-
-US_df.latitude = US_df.latitude.astype('float64')
- 
-US_df.info()
-
-
-
-US_df = pd.get_dummies(US_df, columns=['shape'], prefix_sep='_') 
-US_df['combined'] = list(zip(US_df.latitude, US_df.longitude))
-
-## Correlation table for quantative cereal data
-import seaborn as sns;  # had to add this import
+corr = US_df.corr() # correlation table
 # simple heatmap of correlations (without values)
-corr = US_df.corr()
 sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns)
 # Change the colormap to a divergent scale and fix the range of the colormap
 sns.heatmap(corr, xticklabels=corr.columns, yticklabels=corr.columns, vmin=-1, vmax=1, cmap= "RdBu")
-# Include information about values (example demonstrate how to control the size of
-# the plot
+
+# Include information about values 
 fig, ax = plt.subplots()
 fig.set_size_inches(18, 18)
 sns.heatmap(corr, annot=False, fmt= ".1f", cmap= "RdBu", center=0, ax=ax)
@@ -59,7 +80,9 @@ sns.heatmap(corr, annot=False, fmt= ".1f", cmap= "RdBu", center=0, ax=ax)
 plt.show()
 
 
-## Partitioning Data for Stepwise Regression 
+######### Partitioning Data for Stepwise Regression #########
+from sklearn.model_selection import train_test_split
+
 US_df_loc = US_df.iloc[0:1000]
 US_df_loc.info()
 unwanted = ['city','country', 'latitude', 'longitude','state', 'combined']
@@ -75,24 +98,14 @@ train_X, valid_X, train_y, valid_y = train_test_split(X,y, test_size=0.4, random
 
 
 # print performance measures (training data)
-import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Lasso, Ridge, LassoCV, BayesianRidge
+from sklearn.metrics import roc_curve, auc
 import statsmodels.formula.api as sm
-import matplotlib.pylab as plt
+import math
 
-from dmba import regressionSummary, exhaustive_search
+from dmba import regressionSummary, exhaustive_search, classificationSummary
 from dmba import backward_elimination, forward_selection, stepwise_selection
 from dmba import adjusted_r2_score, AIC_score, BIC_score
-
-import math
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import accuracy_score, roc_curve, auc
-import matplotlib.pylab as plt
-
-from dmba import regressionSummary, classificationSummary
 from dmba import liftChart, gainsChart
 
 
@@ -100,6 +113,7 @@ US_lm = LinearRegression()
 US_lm .fit(train_X, train_y)
 
 regressionSummary(train_y, US_lm.predict(train_X))
+
 def train_model(variables):
     if len(variables) == 0:
         return None
@@ -141,7 +155,6 @@ US_df_loc = US_df.iloc[0:1000]
 #outcome = ['latitude']
 outcome = ['longitude']
 
-from sklearn.model_selection import train_test_split
 
 X = US_df_loc[best_variables]
 y = US_df_loc['latitude']
@@ -149,7 +162,6 @@ X.shape
 y.shape
 train_X, valid_X, train_y, valid_y = train_test_split(X,y, test_size=0.4, random_state=1)
 
-from sklearn.linear_model import LinearRegression
 UFO_lm = LinearRegression()
 UFO_lm.fit(train_X, train_y)
 
@@ -157,7 +169,6 @@ UFO_lm.fit(train_X, train_y)
 print(pd.DataFrame({'Predictor': X.columns, 'coefficient': UFO_lm.coef_}))
 US_df_loc
 
-from dmba import regressionSummary
 regressionSummary(train_y, UFO_lm.predict(train_X))
 
 ## Using Nearest Neighbor
@@ -169,20 +180,23 @@ from sklearn import preprocessing
 from sklearn.neighbors import NearestNeighbors
 
 # Transform the full dataset
-UFO_df = pd.read_csv('/DataMining/lab2/complete.csv')
-low_memory=False
+UFO_df = pd.read_csv('/DataMining/lab2/complete.csv',low_memory=False)
 trainData, validData = train_test_split(US_df, test_size = 0.4, random_state = 1)
-
 
 scaler = preprocessing.StandardScaler()
 scaler.fit(trainData[['shape_changing', 'duration']])
 
 # Transforming dataset
-Norm = pd.concat([pd.DataFrame(scaler.transform(US_df[['shape_changing', 'duration']]), columns=['duration','shape_changing']), US_df[['latitude', 'longitude']]], axis=1)
+Norm = pd.concat([pd.DataFrame(scaler.transform(US_df[['shape_changing', 'duration']]), 
+                               columns=['duration','shape_changing']), 
+                  US_df[['latitude', 'longitude']]], axis=1)
 trainData.index
 
 trainNorm = Norm.iloc[trainData.index]
 validNorm = Norm.iloc[validData.index]
+
+newUFO = pd.DataFrame([{'duration': 300, 'shape_changing': 1}])
+
 newUFONorm = pd.DataFrame(scaler.transform(newUFO),columns=['duration','shape_changing'])
 
 
@@ -210,5 +224,3 @@ for k in range(1,15):
 # Convert results to a pandas df
 results = pd.DataFrame(results)
 print(results)
-
-
